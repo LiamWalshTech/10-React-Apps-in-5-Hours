83e1be2f115f2847d2d758b68669324b9b5dbd52
LiamWalshTech <admin@liamwalsh.tech>
2024-11-07 12:16:44 +0000

    Fixed issues and number 3

--- src/challenges/faq-accordion/index.tsx
+++ src/challenges/faq-accordion/index.tsx
@@ -1,10 +1,89 @@
+import { useState } from "react";
+import classNames from "classnames";
 import styles from "./page.module.css";
 
 function Page() {
+  const [currentTab, setCurrentTab] = useState(1);
+
   return (
     <main className={styles.main}>
       <article className={styles.container}>
-        <h1>FAQ Accordion</h1>
+        <h1 className={styles.title}>FAQs</h1>
+
+        <aside className={styles.accordion}>
+          <section className={styles.accordionSection}>
+            <button
+              className={styles.accordionButton}
+              onClick={() => setCurrentTab(1)}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </button>
+            <p
+              className={classNames(styles.accordionContent, {
+                [styles.accordionActive]: currentTab === 1,
+              })}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              architecto ex ipsam, error quod deleniti alias eos. Deserunt autem
+              ut sint quis laborum aspernatur optio esse rem quae, vero
+              laudantium!
+            </p>
+          </section>
+          <section className={styles.accordionSection}>
+            <button
+              className={styles.accordionButton}
+              onClick={() => setCurrentTab(2)}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </button>
+            <p
+              className={classNames(styles.accordionContent, {
+                [styles.accordionActive]: currentTab === 2,
+              })}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              architecto ex ipsam, error quod deleniti alias eos. Deserunt autem
+              ut sint quis laborum aspernatur optio esse rem quae, vero
+              laudantium!
+            </p>
+          </section>
+          <section className={styles.accordionSection}>
+            <button
+              className={styles.accordionButton}
+              onClick={() => setCurrentTab(3)}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </button>
+            <p
+              className={classNames(styles.accordionContent, {
+                [styles.accordionActive]: currentTab === 3,
+              })}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              architecto ex ipsam, error quod deleniti alias eos. Deserunt autem
+              ut sint quis laborum aspernatur optio esse rem quae, vero
+              laudantium!
+            </p>
+          </section>
+          <section className={styles.accordionSection}>
+            <button
+              className={styles.accordionButton}
+              onClick={() => setCurrentTab(4)}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </button>
+            <p
+              className={classNames(styles.accordionContent, {
+                [styles.accordionActive]: currentTab === 4,
+              })}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              architecto ex ipsam, error quod deleniti alias eos. Deserunt autem
+              ut sint quis laborum aspernatur optio esse rem quae, vero
+              laudantium!
+            </p>
+          </section>
+        </aside>
       </article>
     </main>
   );
--- src/challenges/stats-preview-card-component/index.tsx
+++ src/challenges/stats-preview-card-component/index.tsx
@@ -4,7 +4,40 @@ function Page() {
   return (
     <main className={styles.main}>
       <article className={styles.container}>
-        <h1>Stats</h1>
+        <img
+          className={styles.hero}
+          src="https://plus.unsplash.com/premium_photo-1665203421659-09089ede4ffa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+
+        <section className={styles.content}>
+          <h2 className={styles.title}>
+            Get <span className={styles.primaryLightColour}>insights</span> that
+            help your business grow.
+          </h2>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+            totam consequuntur sequi accusantium! Iusto possimus aspernatur
+            ipsa, accusamus dolorem doloribus vitae obcaecati. Molestiae
+            asperiores modi id eaque voluptates illum cum.
+          </p>
+
+          <ul className={styles.stats}>
+            <li className={styles.stat}>
+              <span className={styles.statValue}>10k+</span>
+              <span className={styles.statCategory}>Companies</span>
+            </li>
+            <li className={styles.stat}>
+              <span className={styles.statValue}>314</span>
+              <span className={styles.statCategory}>Templates</span>
+            </li>
+            <li className={styles.stat}>
+              <span className={styles.statValue}>12M+</span>
+              <span className={styles.statCategory}>Queries</span>
+            </li>
+          </ul>
+        </section>
       </article>
     </main>
   );
